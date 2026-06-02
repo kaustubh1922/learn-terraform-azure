8d23060c9357857bd5fb346a0c08a36d3b0d1deb
kaustubh1922 <kaustubh1922@outlook.com>
2026-06-02 13:37:52 +0530

    Initial commit : Azure Resource group via Terrform

---
+++ main.tf
@@ -0,0 +1,23 @@
+#configure the azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">=1.1.0"
+}
+
+provider "azurerm" {
+  features {
+
+  }
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTfResourceGroup"
+  location = "westus2"
+
+}
